a900ddb126d6798c516a97f3396497ab300e9ac4
Muhammad Abid <adabk9457@gmail.com>
2025-07-10 00:23:41 -0700

    update code

---
+++ lib/controller/constant/appImages/appImages.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Appimages{
+  static const String onboarding_background ='lib/assets/onboarding_background.png';
+  static const String onboarding1_1 ='lib/assets/onboarding1_1.png';
+  static const String onboarding1_2 ='lib/assets/onboarding1_2.png';
+  static const String onboarding2_1 ='lib/assets/onboarding2_1.png';
+  static const String onboarding2_2 ='lib/assets/onboarding2_2.png';
+  static const String onboarding3_1 ='lib/assets/onboarding3_1.png';
+  static const String onboarding3_ ='lib/assets/onboarding3_2.png';
+  static const String onboarding4_1 ='lib/assets/onboarding4_1.png';
+  static const String onboarding4_2 ='lib/assets/onboarding4_2.png';
+}
---
+++ lib/controller/constant/appicons/appIcons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Appicons{
+  static const String email_icon='lib/assets/email_icon.png';
+  static const String password_icon='lib/assets/password_icon.png';
+  static const String close_eye_icon='close_eye_icon.png';
+}
