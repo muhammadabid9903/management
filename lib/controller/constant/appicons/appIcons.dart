d2f55fef479e7c84ad77f2e2c70409fb0a70bf7f
Muhammad Abid <adabk9457@gmail.com>
2025-07-10 00:40:46 -0700

    update

--- lib/controller/constant/appicons/appIcons.dart
+++ lib/controller/constant/appicons/appIcons.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class Appicons{
   static const String email_icon='lib/assets/email_icon.png';
   static const String password_icon='lib/assets/password_icon.png';
-  static const String close_eye_icon='close_eye_icon.png';
+  static const String close_eye_icon='lib/assets/close_eye_icon.png';
+  static const String id_icon ='lib/assets/id_icon.png';
 }
